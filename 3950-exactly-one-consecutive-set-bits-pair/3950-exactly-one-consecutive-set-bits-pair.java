f7a90a53f85f7316c47eac48b7bc973cba36d517
KUSHAWAHA ANKITKUMAR <117963120+Ankit4745@users.noreply.github.com>
2026-06-10 07:43:58 +0530

    Time: 1 ms (98.97%), Space: 42.5 MB (65.36%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean consecutiveSetBits(int n) {
+          int cnt = 0;
+        while(n != 0){
+            if((n & 3) == 3){
+                cnt++;
+            }
+            n = n >> 1;
+        }
+        return cnt == 1;
+    }
+}
+ 
